592e5b7939807d8cf2e18a22f302294b41f6600f
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-09-17 19:22:29 +0300

    Complete Assignment

---
+++ index.js
@@ -0,0 +1,73 @@
+ function add(){
+    return 1 + 2;
+}
+console.log(add())
+
+function subtract(){
+    return 314 - 0
+}
+console.log(subtract())
+
+function multiply(){
+    return 5 * 10;
+}
+
+function divide(){
+    return 30 / 5;
+}
+console.log(divide())
+
+
+function add(a, b){
+    return a + b;
+}
+console.log(add(1000, 127))
+
+
+function subtract(a, b){
+    return a - b;
+}
+console.log(subtract(10, 5))
+
+
+function multiply(a, b){
+    return a * b;
+}
+console.log(multiply(10, 5));
+
+function divide(a, b){
+    return a / b
+}
+console.log(divide(10, 2)) 
+
+function increment(n){
+    return ++n;
+}
+console.log(increment(6));
+
+function decrement(n){
+    return --n;
+}
+console.log(decrement(5));
+
+function makeInt(n){
+    return parseFloat(n);
+}
+console.log(makeInt(3))
+
+function makeInt(n){
+    return parseInt(n, 10);
+} 
+console.log(makeInt(5))
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+console.log(preserveDecimal(3.4555));
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+console.log(preserveDecimal("3.4555"));
+
+
